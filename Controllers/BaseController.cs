45a048f4a962f68fe1c6a825b14c7dd2e39cdcda
Daniel <31039508+ranmas2010@users.noreply.github.com>
2024-07-18 15:01:54 +0800

    Initial commit

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebTest.Models;
+
+namespace WebTest.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly ILogger<BaseController> _logger;
+
+        public BaseController(ILogger<BaseController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
